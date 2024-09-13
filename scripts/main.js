174c8988305beaac364c82dee926cf91c79c1d95
Justin <jal1068@wgu.edu>
2024-09-13 12:30:08 -0700

    some layout changes and added scripts

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+const rngButton = document.querySelector('#rng-button');
+const rngDisplay = document.querySelector('#random-number');
+
+//rngButton.textContent = "Test";
+
+console.log("Test");
+
+function rng() {
+    console.log(Math.random());
+}
